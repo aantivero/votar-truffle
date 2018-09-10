2abf12d4bdbed7d5936530f846e63bda40286bd8
alejandroantivero <alejandro.antivero@gmail.com>
2018-09-09 23:49:31 -0300

    Se crea la estructura para almacenar un candidato, un contador de cantidados y una función para crear candidatos de manero privada

--- contracts/Eleccion.sol
+++ contracts/Eleccion.sol
@@ -1,12 +1,31 @@
 pragma solidity ^0.4.24;
 
 contract Eleccion {
-    //guardar candidato
-    //leer candidato por definicion al ser publica ya tiene getter
-    string public candidato;
+
+    // modelar un candidato
+    struct Candidato {
+        uint id;
+        string nombre;
+        uint votos;
+    }
+    // guardar y obtener candidatos
+    mapping(uint => Candidato) public candidatos;
+
+    // guardar la cantidad de candidatos
+    //en solidity no podemos saber el tamaño del array y no se puede iterar
+    //no se puede saber el tamaño de candidatos
+    uint public cantidadCandidatos;
+
 
     //constructor
     constructor() public {
-        candidato = "Candidato 1";
+        agregarCandidato("Juan");
+        agregarCandidato("Pedro");
+    }
+
+    //esta función es accesible desde fuera
+    function agregarCandidato(string _nombre) private {
+        cantidadCandidatos ++;
+        candidatos[cantidadCandidatos] = Candidato(cantidadCandidatos, _nombre, 0);
     }
 }
