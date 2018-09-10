3e32022b772e3c523299af07c36878780c182c99
alejandroantivero <alejandro.antivero@gmail.com>
2018-09-10 00:18:40 -0300

    Test si iniciaron correctamente los datos de los candidatos

--- test/eleccion.js
+++ test/eleccion.js
@@ -1,12 +1,30 @@
 var Eleccion = artifacts.require('./Eleccion.sol');
 
-contract("Eleccion", function(accounts) {
+contract("Eleccion", function(cuentas) {
+
+    var eleccionInstancia;
 
     it("Inicializar con solo 2 candidatos", function() {
-        return Eleccion.deployed().then(function (instance) {
-            return instance.cantidadCandidatos();
+        return Eleccion.deployed().then(function (instancia) {
+            return instancia.cantidadCandidatos();
         }).then(function(cantidad) {
             assert.equal(cantidad, 2);
+        });
+    });
+
+    it("Inician con los valores correctos", function() {
+        return Eleccion.deployed().then(function (instancia) {
+            eleccionInstancia = instancia;
+            return eleccionInstancia.candidatos(1);
+        }).then(function(candidato) {
+            assert.equal(candidato[0], 1, 'id correcto');
+            assert.equal(candidato[1], 'Juan', 'nombre correcto');
+            assert.equal(candidato[2], 0, 'votos correctos');
+            return eleccionInstancia.candidatos(2);
+        }).then(function(candidato) {
+            assert.equal(candidato[0], 2, 'id correcto');
+            assert.equal(candidato[1], 'Pedro', 'nombre correcto');
+            assert.equal(candidato[2], 0, 'votos correctos');
         });
     });
 });
